89063bb05cde369f99e42d4ab4225375cc174111
marbin701 <marvin07012002@gmail.com>
2026-05-20 15:12:18 -0400

    working in FixedList

--- src/main/java/com/pluralsight/collection/FixedList.java
+++ src/main/java/com/pluralsight/collection/FixedList.java
@@ -3,19 +3,29 @@ package com.pluralsight.collection;
 import java.util.ArrayList;
 
 public class FixedList<E> {
-    ArrayList<E> items = new ArrayList<>();
-    int maxSize;
+    private ArrayList<E> items = new ArrayList<>();
+    private int maxSize;
 
 
     public FixedList(int maxSize) {
         this.maxSize = maxSize;
     }
 
-    public E get(int index) {
-        return null;
-    }
-
     public void add(E item) {
-
+        if (items.size() < maxSize) {
+            items.add(item);
+        } else {
+            throw new IndexOutOfBoundsException("You can't add more than " + this.maxSize + " items, mate");
+        }
     }
+
+    public E get(int index) {
+        if (index > (maxSize - 1)){
+            throw new IndexOutOfBoundsException("This collection can't contain more than " + this.maxSize+ " items");
+        }
+
+            return null;
+    }
+
+
 }
